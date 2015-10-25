18a226b313ebe2d7a4344faf5de2eff86d0f0dee
clindsay107 <clindsay107@gmail.com>
2015-10-25 14:34:59 -0700

    Add snippets file

---
+++ misc/snippets.sh
@@ -0,0 +1,8 @@
+# Helpful commands or snippets that would be nice to copy/paste
+
+##
+## IRC
+##
+
+# Set up screen, with colors, for SICP mode
+screen -T screen-256color -S sicp irssi --config=~/code/dotfiles/irssi/sicp_config
