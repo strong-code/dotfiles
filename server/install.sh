a1eda15e2dc8b61c5a84c09a3230e1318536ba6d
strong-code <colin@strongco.de>
2016-01-08 11:21:48 -0800

    Consolidate server install script

--- server/install.sh
+++ server/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 apt-get update # Refresh package list
-apt-get install htop -y
-apt-get install fail2ban -y
-apt-get install python-pip -y
+apt-get install build-essential -y
+
+apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev -y
+apt-get install tk-dev libgdbm-dev libc6-dev libbz2-dev -y
+apt-get install python-pip htop fail2ban libffi-dev -y
