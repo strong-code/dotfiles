d0c7edccebf9b0ff6bea787b258d495ab107255f
strong-code <colin@strongco.de>
2016-02-08 10:52:09 -0800

    Add sqlite to server install

--- server/install.sh
+++ server/install.sh
@@ -7,4 +7,4 @@ apt-get update # Refresh package list
 
 apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev -y
 apt-get install tk-dev libgdbm-dev libc6-dev libbz2-dev python-software-properties -y
-apt-get install python-pip htop fail2ban libffi-dev -y
+apt-get install python-pip htop fail2ban libffi-dev sqlite3 -y
