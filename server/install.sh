0716f4d3e4e892142be2d3f505a31790249bd032
strong-code <colin@strongco.de>
2016-02-10 12:39:07 -0800

    Update server install script and add ghost install

--- server/install.sh
+++ server/install.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
+echo "Beginning server install script..."
 apt-get install build-essential -y
-
-sudo add-apt-repository ppa:keithw/mosh
 
 apt-get update # Refresh package list
 
+echo "Up to date! Installing dependency packages now..."
 apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev -y
 apt-get install tk-dev libgdbm-dev libc6-dev libbz2-dev python-software-properties -y
-apt-get install python-pip htop fail2ban libffi-dev sqlite3 -y
+apt-get install python-pip htop libffi-dev sqlite3 zip wget -y
+
+# Download and install NVM, Node and NPM
+echo "Finished! Installing NVM, Node.js and NPM..."
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+
+source ~/.bash_profile
+source ~/.profile
+source ~/.bash_rc
+
+nvm install 0.12.9
+nvm install 4.2
+
+nvm use 0.12.9
+
+apt-get install npm -y
+
+# install ghost blogging platform via separate script
+echo "Finished! Installing ghost platform..."
+sh ./install-ghost.sh
+echo "Finished!"
