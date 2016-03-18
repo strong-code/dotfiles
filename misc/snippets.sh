be29c1d583a38ad08e38423694101f22231a321c
strong-code <colin@strongco.de>
2016-03-18 11:12:32 -0700

    add easy error log grep for day snippet

--- misc/snippets.sh
+++ misc/snippets.sh
@@ -9,3 +9,7 @@ screen -T screen-256color -S irc irssi
 
 # Set up named screen, with colors, for SICP mode
 screen -T screen-256color -S sicp irssi --config=~/code/dotfiles/irssi/sicp_config
+
+# Grep etherpulse job_errors log file for all errors for day from root dir
+forDay= date +%Y-%m-%d
+grep "$forDay" ./log/job_errors.log
