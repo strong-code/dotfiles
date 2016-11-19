8a96537cbd76556f3353f5c237f8a80561f26a60
strong-code <clindsay107@gmail.com>
2016-11-18 16:20:59 -0800

    no rvm

--- ruby/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo Installing Ruby stable from RVM...
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-echo Done! Loading RVM script now...
-source /Users/chl/.rvm/scripts/rvm
-
-echo Done! RVM is ready to use!
