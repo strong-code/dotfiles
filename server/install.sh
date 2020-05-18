7fc20e119a901a6d035c655f085ea0a228d0e2f2
strong-code <clindsay107@gmail.com>
2020-05-17 19:21:16 -0700

    server: server installation script now sets up dir and links authorized_keys and crontab

--- server/install.sh
+++ server/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "Setting up dir structure"
+mkdir -p ~/Documents/code
+
 echo "Beginning server install script..."
 apt-get install build-essential -y
 
@@ -7,7 +10,16 @@ apt-get update # Refresh package list
 echo "Up to date! Installing dependency packages now..."
 apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev -y
 apt-get install tk-dev libgdbm-dev libc6-dev libbz2-dev python-software-properties -y
-apt-get install python-pip htop libffi-dev sqlite3 zip wget pip bundler xclip rtorrent -y
+apt-get install python-pip htop libffi-dev sqlite3 zip wget pip bundler xclip -y
+
+echo "Pulling latest dotfiles..."
+cd ~/Documents/code
+git clone git@github.com:strong-code/dotfiles.git
+
+echo "Linking authorized_keys and crontab..."
+ln -s ~/Documents/code/dotfiles/server/authorized_keys ~/.ssh/authorized_keys
+rm /var/spool/cron/crontab/root
+ln -s ~/Documents/code/dotfiles/server/crontab /var/spool/cron/crontab/root
 
 # Download and install NVM, Node and NPM
 echo "Finished! Installing NVM, Node.js and NPM..."
@@ -23,13 +35,6 @@ nvm install 6.1
 nvm alias default node
 
 apt-get install npm -y
-
-echo "Setting up rtorrent..."
-if [ ! -d ~/rtorrent ]; then
-  mkdir ~/rtorrent
-fi
-
-ln -s ~/Documents/code/dotfiles/server/.rtorrent.rc ~/.rtorrent.rc
 
 echo "Linking Nginx site configs..."
 ln -s ~/Documents/code/dotfiles/server/sites-enabled /etc/nginx/
